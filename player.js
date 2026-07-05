e54fc34149666dae1d2b9092eedc9bce62c0a5b6
chiru199231 <srai95273@gmail.com>
2026-07-05 11:55:09 +0530

    Add ticket data for Brazil and Norway players

---
+++ player.js
@@ -0,0 +1,279 @@
+const tickets = [
+
+{
+ticket:1,
+brazil:{
+name:"Igor Thiago",
+number:25
+},
+norway:{
+name:"Sondre Langas",
+number:24
+}
+},
+
+{
+ticket:2,
+brazil:{
+name:"Lucas Paqueta",
+number:20
+},
+norway:{
+name:"Henrik Falchener",
+number:27
+}
+},
+
+{
+ticket:3,
+brazil:{
+name:"Endrick",
+number:19
+},
+norway:{
+name:"Leo Ostigard",
+number:4
+}
+},
+
+{
+ticket:4,
+brazil:{
+name:"Vinicius Junior",
+number:7
+},
+norway:{
+name:"Kristoffer Ajer",
+number:3
+}
+},
+
+{
+ticket:5,
+brazil:{
+name:"Danilo Santos",
+number:18
+},
+norway:{
+name:"David Moller Wolfe",
+number:5
+}
+},
+
+{
+ticket:6,
+brazil:{
+name:"Bruno Guimaraes",
+number:8
+},
+norway:{
+name:"Fredrik Bjorkan",
+number:15
+}
+},
+
+{
+ticket:7,
+brazil:{
+name:"Fabinho",
+number:17
+},
+norway:{
+name:"Marcus Holmgren Pedersen",
+number:16
+}
+},
+
+{
+ticket:8,
+brazil:{
+name:"Matheus Cunha",
+number:9
+},
+norway:{
+name:"Sander Berge",
+number:8
+}
+},
+
+{
+ticket:9,
+brazil:{
+name:"Bremer",
+number:14
+},
+norway:{
+name:"Fredrik Aursnes",
+number:14
+}
+},
+
+{
+ticket:10,
+brazil:{
+name:"Ederson Silva",
+number:2
+},
+norway:{
+name:"Andreas Schjelderup",
+number:21
+}
+},
+
+// Continue tickets 11–23 in exactly the same format...
+
+];{
+ticket:11,
+brazil:{
+name:"Danilo",
+number:13
+},
+norway:{
+name:"Martin Odegaard",
+number:10
+}
+},
+
+{
+ticket:12,
+brazil:{
+name:"Alex Sandro",
+number:6
+},
+norway:{
+name:"Patrick Berg",
+number:6
+}
+},
+
+{
+ticket:13,
+brazil:{
+name:"Marquinhos",
+number:4
+},
+norway:{
+name:"Oscar Bobb",
+number:22
+}
+},
+
+{
+ticket:14,
+brazil:{
+name:"Gabriel Magalhaes",
+number:3
+},
+norway:{
+name:"Morten Thorsby",
+number:2
+}
+},
+
+{
+ticket:15,
+brazil:{
+name:"Roger Ibanez",
+number:24
+},
+norway:{
+name:"Thelo Aasgaard",
+number:19
+}
+},
+
+{
+ticket:16,
+brazil:{
+name:"Douglas Santos",
+number:16
+},
+norway:{
+name:"Torbjorn Heggem",
+number:17
+}
+},
+
+{
+ticket:17,
+brazil:{
+name:"Leo Pereira",
+number:15
+},
+norway:{
+name:"Antonio Nusa",
+number:20
+}
+},
+
+{
+ticket:18,
+brazil:{
+name:"Neymar Jr",
+number:10
+},
+norway:{
+name:"Jens Petter Hauge",
+number:23
+}
+},
+
+{
+ticket:19,
+brazil:{
+name:"Casemiro",
+number:5
+},
+norway:{
+name:"Kristian Thorstvedt",
+number:18
+}
+},
+
+{
+ticket:20,
+brazil:{
+name:"Rayan",
+number:26
+},
+norway:{
+name:"Erling Haaland",
+number:9
+}
+},
+
+{
+ticket:21,
+brazil:{
+name:"Gabriel Martinelli",
+number:22
+},
+norway:{
+name:"Alexander Sorloth",
+number:7
+}
+},
+
+{
+ticket:22,
+brazil:{
+name:"Raphinha",
+number:11
+},
+norway:{
+name:"Julian Ryerson",
+number:26
+}
+},
+
+{
+ticket:23,
+brazil:{
+name:"Luiz Henrique",
+number:21
+},
+norway:{
+name:"Jorgen Strand Larsen",
+number:11
+}
+}
